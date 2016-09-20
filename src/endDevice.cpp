d91cd79e04d6512c655e564e5bfdb0e9fcf3e4b6
ykhorizon <ykhorizon.light@gmail.com>
2016-09-20 17:05:45 +0800

    first working version

--- src/endDevice.cpp
+++ src/endDevice.cpp
@@ -6,27 +6,33 @@
  */
 #include "Arduino.h"
 #include "SoftwareSerial.h"
-#define ec5_a2 2
-#define ec5_a1 1
-#define ec5_a0 0
+#define EC5_A2 2  // 5cm depth
+#define EC5_A1 1  // 10cm depth
+#define EC5_A0 0  // 15cm depth
+#define VALVE_D8 8 // electric valve
 void setup()
 {
   Serial.begin(9600);
 
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(VALVE_D8, OUTPUT);
 }
 
 void loop()
 {
 
+  // open valve for 15 sec
+  digitalWrite(VALVE_D8, HIGH);
+  delay(15000);
+  digitalWrite(VALVE_D8, LOW);
 
-   // wait for a second
-  delay(1000);
+  // wait for minutes
+  delay(60000);
 
-  float v0 = analogRead(ec5_a0)*(3.3/1023.0);
-  float v1 = analogRead(ec5_a1)*(3.3/1023.0);
-  float v2 = analogRead(ec5_a2)*(3.3/1023.0);
+  float v0 = analogRead(EC5_A0)*(3.3/1023.0);
+  float v1 = analogRead(EC5_A1)*(3.3/1023.0);
+  float v2 = analogRead(EC5_A2)*(3.3/1023.0);
   Serial.print("ec5,a0,");
   Serial.print(v0);
   Serial.print(",");
---
+++ src/piSide.py
@@ -0,0 +1,66 @@
+import serial
+import time
+import datetime
+import sys
+
+# Calibration function ( for 2500mV )
+def potting_calibration( mV ):
+    return 2.11 * (10**-3) * mV - 0.675
+# Dielectric Permittivity ( for 2500mV )
+def dp( mV ):
+    return 1/( -3.3326*10**-9 * mV**3 + 7.0218*10**-6 * mV**2 -\
+    5.11647*10**-3 * mV + 1.30746 )
+
+def get_mV_index( AnalogId ):
+    if AnalogId >= 0 :
+        return (id+1)*3-1;
+    else:
+        return null;
+
+def get_current_datetime():
+    ts = time.time()
+    return datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H_%M_%S')
+
+
+# Retrieve information
+record_cnt = 1000
+skip_cnt = 5 #skip first 5 datas for maintain data quality
+
+devPath = '/dev/ttyACM0'
+# devPath = '/dev/bus/usb/001/005'
+# Serial initialize
+ser = serial.Serial(devPath, 9600, timeout=1)
+
+
+if sys.argv[1] != '--test':
+    # File open initialize
+    dataDir = "./data/{}.csv".format( get_current_datetime() )
+    fp = open(dataDir, "w+")
+    fp.write( "#record_cnt: {}\n".format(record_cnt) )
+
+# Variales initialize
+i = 0
+rawStr = ''
+
+
+try:
+    while i < record_cnt + skip_cnt:
+        string = ser.readline()
+        i+=1
+        if string and i > skip_cnt:
+
+            rawStr = string.rstrip()
+            strList = rawStr.split(',')
+            # sensorVolt = float(strList[2])
+            print '--------- {}, cnt: {} ---------'.format( get_current_datetime(), i )
+            print rawStr
+            if sys.argv[1] != '--test':
+                fp.write("{}\n".format(rawStr))
+            # fp.write("{},{},{}\n".format (strList[6],strList[7],strList[8]))
+
+
+
+except Exception as e:
+    raise
+if sys.argv[1] != '--test':
+    fp.close()
